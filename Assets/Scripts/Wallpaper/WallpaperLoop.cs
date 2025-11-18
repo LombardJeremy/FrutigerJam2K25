185a7341a5d6b7ef4854e80e69b6c8756f162ed3
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2025-11-18 15:48:34 +0100

    NEW : add end walk to walk to an other test

--- Assets/Scripts/Wallpaper/WallpaperLoop.cs
+++ Assets/Scripts/Wallpaper/WallpaperLoop.cs
@@ -5,15 +5,20 @@ public class WallpaperLoop : MonoBehaviour
 
     [SerializeField] private Transform pointsToMove;
 
+    int indexPoint = 0;
+
     void Start()
     {
         AssistantBehaviour.instance.onMoveFinish.AddListener(onEndWalk);
-        AssistantBehaviour.instance.MoveTo(pointsToMove.GetChild(0).position);
+        AssistantBehaviour.instance.MoveTo(pointsToMove.GetChild(indexPoint).position);
     }
 
     void onEndWalk()
     {
         AssistantBehaviour.instance.LookAt(Vector3.zero);
+        indexPoint++;
+
+        if (indexPoint < pointsToMove.childCount) AssistantBehaviour.instance.MoveTo(pointsToMove.GetChild(indexPoint).position);
     }
 
     void Update()
